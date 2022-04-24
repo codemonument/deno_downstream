6b947b3341cab458f8ab7f8016ad5bc4c74bcf7f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 19:51:43 +0200

    make progress stream mandatory

--- lib/DownstreamResponse.ts
+++ lib/DownstreamResponse.ts
@@ -9,7 +9,7 @@ export interface DownstreamResponse {
 
   // TODO: Figure out, how to implement a progress stream in percent
   // see: https://medium.com/deno-the-complete-reference/readable-streams-in-deno-e5d707735a77
-  progress?: ReadableStream<number>;
+  progress: ReadableStream<number>;
 
   /**
    * Should be set to a method for closing the body and progress stream simultaneously
