ed26838716d15bce30222a21fb074986c262cad9
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 22:26:39 +0200

    add local fork of progress library

--- dependencies/_progressbar.ts
+++ dependencies/_progressbar.ts
@@ -1,2 +1,5 @@
-export * from "https://deno.land/x/progress@v1.2.4/mod.ts";
-export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.4/mod.ts";
+// export * from "https://deno.land/x/progress@v1.2.4/mod.ts";
+// export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.4/mod.ts";
+
+export * from "./progress-fork/mod.ts";
+export { default as ProgressBar } from "./progress-fork/mod.ts";
