c30c06c3f2eb6fd1ea131884efedc65b11b04288
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 21:33:58 +0200

    update testfile

--- lib/downstream.test.ts
+++ lib/downstream.test.ts
@@ -1,5 +1,6 @@
 import {
   assert,
+  assertRejects,
   assertSnapshot,
   describe,
   it,
@@ -8,25 +9,27 @@ import { downstream } from "../mod.ts";
 
 const File1GB = `https://speed.hetzner.de/1GB.bin`;
 const File100MB = `https://speed.hetzner.de/100MB.bin`;
-const File50MB = `https://speed.hetzner.de/50MB.bin`;
+const File50MB = `http://ipv4.download.thinkbroadband.com/50MB.zip`;
+const File50MB404 = `https://speed.hetzner.de/50MB.bin`;
 
 /**
  * tc = (Deno) test context
  */
-describe(`'downstream' Function`, () => {
+describe(`'downstream'`, () => {
   it("returns content size correctly", async () => {
     const { contentLength, closeStreams } = await downstream(File1GB);
+
     console.log(contentLength);
-    assert(typeof contentLength === "number");
     const kb = contentLength / 1024;
     const mb = kb / 1024;
     const gb = mb / 1000; // the 1GB testfile consists of 1000 mb
-    assert(gb === 1);
     closeStreams();
+    assert(typeof contentLength === "number");
+    assert(gb === 1);
   });
 
   it.ignore(`Reports Progress correctly`, async (tc) => {
-    const { progressStream, closeStreams } = await downstream(File50MB);
+    const { progressStream, closeStreams } = await downstream(File100MB);
 
     const progressEvents: string[] = [];
     for await (const progress of progressStream) {
@@ -37,4 +40,9 @@ describe(`'downstream' Function`, () => {
 
     await assertSnapshot(tc, progressEvents);
   });
+});
+
+describe(`'downstream' Regression Tests`, () => {
+  it(`should not leak streams on http errors (like 404)`, () =>
+    assertRejects(() => downstream(File50MB404)));
 });
