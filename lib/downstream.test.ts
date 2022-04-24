1e6ed887c6aad644c6a8ce065e539075acb03ae2
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 12:08:07 +0200

    add a first basic test for downstream function

---
+++ lib/downstream.test.ts
@@ -0,0 +1,13 @@
+import { assert, describe, it } from "../dependencies/_testing.std.ts";
+import { downstream } from "../mod.ts";
+
+const File1GB = `https://speed.hetzner.de/1GB.bin`;
+
+describe(`'downstream' Function`, () => {
+  it("returns content size correctly", async () => {
+    const result = await downstream(File1GB);
+    console.log(result.contentLength);
+    assert(typeof result.contentLength === "number");
+    result.body.cancel();
+  });
+});
