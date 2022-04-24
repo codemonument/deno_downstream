8bf499df1ec55f2b044e868043a00105345524ff
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 19:57:42 +0200

    assert filesize of GB file

--- lib/downstream.test.ts
+++ lib/downstream.test.ts
@@ -2,12 +2,17 @@ import { assert, describe, it } from "../dependencies/_testing.std.ts";
 import { downstream } from "../mod.ts";
 
 const File1GB = `https://speed.hetzner.de/1GB.bin`;
+const File100MB = `https://speed.hetzner.de/100MB.bin`;
 
 describe(`'downstream' Function`, () => {
   it("returns content size correctly", async () => {
     const result = await downstream(File1GB);
     console.log(result.contentLength);
     assert(typeof result.contentLength === "number");
+    const kb = result.contentLength / 1024;
+    const mb = kb / 1024;
+    const gb = mb / 1000; // the 1GB testfile consists of 1000 mb
+    assert(gb === 1);
     result.closeStreams();
   });
 });
