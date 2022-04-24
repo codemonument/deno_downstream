600db4eb58980ed294d17c127005a9d06543c6d9
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 13:38:19 +0200

    add method to DownstreamResponse to close all streams at once

--- lib/DownstreamResponse.ts
+++ lib/DownstreamResponse.ts
@@ -10,4 +10,9 @@ export interface DownstreamResponse {
   // TODO: Figure out, how to implement a progress stream in percent
   // see: https://medium.com/deno-the-complete-reference/readable-streams-in-deno-e5d707735a77
   progress?: ReadableStream<number>;
+
+  /**
+   * Should be set to a method for closing the body and progress stream simultaneously
+   */
+  closeStreams: () => void;
 }
