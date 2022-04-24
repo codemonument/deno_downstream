c84b882c17f2f5de7a34f6256f6df3461f4ad37b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 20:09:15 +0200

    add precision to progressTransformerOptions

--- lib/progressTransformer.ts
+++ lib/progressTransformer.ts
@@ -1,15 +1,29 @@
+export interface ProgressTransformerOptions {
+  maxBytes: number;
+
+  /**
+   * The number of digits which should be used after the comma
+   * Defaults to 2 (example: 10.68 Percent)
+   */
+  precision?: number;
+}
+
 /**
  * Creates a Stream Transformer to convert a ReadableStream into a Progress Stream for the download progress in percent
- * @param maxBytes the byte count of the file download
  */
-export function createProgressTransformer(maxBytes: number) {
+export function createProgressTransformer(
+  { maxBytes, precision = 2 }: ProgressTransformerOptions,
+) {
   let writtenBytes = 0;
   const progressTransformer = new TransformStream<Uint8Array, number>({
     start() {},
     transform(chunk, controller) {
       writtenBytes += chunk.length;
       const progressPercent = (writtenBytes / maxBytes) * 100;
-      controller.enqueue(progressPercent);
+      const progressWithPrecision = Number.parseFloat(
+        progressPercent.toFixed(precision),
+      );
+      controller.enqueue(progressWithPrecision);
     },
     flush() {},
   });
