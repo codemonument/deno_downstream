630b3bb41f8b2055c03fa4cb7f0adde504176ac6
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 22:38:18 +0200

    add drainProgressStream function

---
+++ lib/_testutils/drainProgressStream.ts
@@ -0,0 +1,14 @@
+import { ProgressBar } from "../../dependencies/_progressbar.ts";
+
+export async function drainProgressStream(stream: ReadableStream<string>) {
+  const progressBar = new ProgressBar({
+    title: "Draining Stream: ",
+    total: 100,
+  });
+
+  for await (const progress of stream) {
+    progressBar.render(Number.parseFloat(progress));
+  }
+
+  stream.cancel();
+}
