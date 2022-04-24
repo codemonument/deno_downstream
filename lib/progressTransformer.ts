ac42c76bf02aa3ae570866bc32fa29627577cde6
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 13:14:39 +0200

    wrap progressTransformer into a factory function

--- lib/progressTransformer.ts
+++ lib/progressTransformer.ts
@@ -1,8 +1,16 @@
-export const progressTransformer = new TransformStream({
-  start(controller) {
-  },
-  transform(chunk, controller) {
-  },
-  flush(controller) {
-  },
-});
+/**
+ * Creates a Stream Transformer to convert a ReadableStream into a Progress Stream for the download progress in percent
+ * @param bytes the byte count of the file download
+ */
+export function createProgressTransformer(bytes: number) {
+  const progressTransformer = new TransformStream({
+    start(controller) {
+    },
+    transform(chunk, controller) {
+    },
+    flush(controller) {
+    },
+  });
+
+  return progressTransformer;
+}
