038c93ebf40bd16d20054cd5d6ef49c5577307db
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-24 22:18:59 +0200

    add extra file for testfile urls

---
+++ test/testfiles.ts
@@ -0,0 +1,4 @@
+export const File1GB = `https://speed.hetzner.de/1GB.bin`;
+export const File100MB = `https://speed.hetzner.de/100MB.bin`;
+export const File50MB = `http://ipv4.download.thinkbroadband.com/50MB.zip`;
+export const File50MB404 = `https://speed.hetzner.de/50MB.bin`;
