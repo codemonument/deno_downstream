b2d80cddb3132abcf3f6a0bf7b8ab962e34942b7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-04 18:45:47 +0100

    add test for correct content length

--- test/downstream.test.ts
+++ test/downstream.test.ts
@@ -29,18 +29,32 @@ Deno.test(`'downstream' function`, async (tc) => {
   });
 
   await tc.step(`Runs tests correctly (consuming streams)`, async () => {
-    const { closeStreams, fileStream, contentLength } = await downstream(
+    const { fileStream, contentLength } = await downstream(
       File50MB,
     );
 
     assert(typeof contentLength === "number");
     assert(contentLength > 0);
 
+    // needs to be done to not crash deno test
     await drainStream(fileStream);
 
     // This doesn't work for some reason
     // TODO: => why do I not have to close the progressStream after draining the fileStream???
     // await closeStreams();
+  });
+
+  await tc.step(`Returns content size correctly`, async () => {
+    const { fileStream, contentLength } = await downstream(
+      File50MB,
+    );
+
+    const kb = contentLength / 1024;
+    const mb = kb / 1024;
+    assert(mb === 50); // the 50MB testfile consists of 50MB
+
+    // needs to be done to not crash deno test
+    await drainStream(fileStream);
   });
 });
 
