af37eff5ddf1583304644700ee3e86582e3a9b03
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-05 21:13:42 +0100

    go back to 1.2.4 version of progress bar with the fix for running in deno tests, until my PR gets merged

--- dependencies/_progressbar.ts
+++ dependencies/_progressbar.ts
@@ -1,7 +1,6 @@
-// export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.4/mod.ts";
+export * from "./progress-fork/mod.ts";
+export { default as ProgressBar } from "./progress-fork/mod.ts";
 
-// export * from "./progress-fork/mod.ts";
-// export { default as ProgressBar } from "./progress-fork/mod.ts";
-
-export * from "https://deno.land/x/progress@v1.2.8/mod.ts";
-export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.8/mod.ts";
+// can be used in deno tests when this PR gets merged: https://github.com/deno-library/progress/pull/14
+// export * from "https://deno.land/x/progress@v1.2.8/mod.ts";
+// export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.8/mod.ts";
