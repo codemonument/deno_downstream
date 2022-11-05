e7456c2dce5c09ef4b4de807a0f659fb246af9b8
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-05 22:00:57 +0100

    make simpleStreamCallback test faster

--- test/units/simpleStreamCallback.test.ts
+++ test/units/simpleStreamCallback.test.ts
@@ -2,7 +2,7 @@ import { simpleTimerStream } from "@deps/simpleTimerStream.ts";
 import { simpleStreamCallback } from "@mod";
 
 Deno.test(`simpleStreamCallback Adapter`, async (tc) => {
-  await simpleTimerStream({ intervalInMilliseconds: 500 }).pipeTo(
+  await simpleTimerStream({ intervalInMilliseconds: 100 }).pipeTo(
     simpleStreamCallback((chunk) => {
       console.log(chunk);
     }),
