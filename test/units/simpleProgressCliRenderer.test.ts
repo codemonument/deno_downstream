eb2bb083d4007c96e526f2f5e32ea2dcc682d015
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-07 22:41:40 +0100

    Simplify simpleProgressCliRenderer test

--- test/units/simpleProgressCliRenderer.test.ts
+++ test/units/simpleProgressCliRenderer.test.ts
@@ -1,24 +1,21 @@
 import { simpleProgressCliRenderer } from "@mod";
 import { simpleTimerStream } from "@deps/simpleTimerStream.ts";
 
-Deno.test(`simpleProgressCliRenderer Adapter`, async (tc) => {
-  await tc.step(`should output a simple progress bar`, async () => {
-    await simpleTimerStream({ maxEventCount: 100, intervalInMilliseconds: 25 })
-      .pipeTo(
-        simpleProgressCliRenderer(),
-      );
-  });
+// Deno.test(``, async () => {});
 
-  await tc.step(
-    `should accept a custom title for the progressbar`,
-    async () => {
-      await simpleTimerStream({
-        maxEventCount: 100,
-        intervalInMilliseconds: 25,
-      })
-        .pipeTo(
-          simpleProgressCliRenderer({ title: "My Custom Bar Title" }),
-        );
-    },
-  );
+Deno.test(`Should output a simple progress bar`, async () => {
+  await simpleTimerStream({ maxEventCount: 100, intervalInMilliseconds: 25 })
+    .pipeTo(
+      simpleProgressCliRenderer(),
+    );
+});
+
+Deno.test(`Should accept a custom title for the progressbar`, async () => {
+  await simpleTimerStream({
+    maxEventCount: 100,
+    intervalInMilliseconds: 25,
+  })
+    .pipeTo(
+      simpleProgressCliRenderer({ title: "My Custom Bar Title" }),
+    );
 });
