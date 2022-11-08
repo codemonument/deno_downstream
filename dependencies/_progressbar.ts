a452f7f00d8884652d92f579c6eb35e1c8f88728
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 05:27:12 +0100

    update progressbar dep to 1.3.3

--- dependencies/_progressbar.ts
+++ dependencies/_progressbar.ts
@@ -1,3 +1,3 @@
 // update to >1.3.0 when pr is merged
-export * from "https://deno.land/x/progress@v1.2.9/mod.ts";
-export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.9/mod.ts";
+export * from "https://deno.land/x/progress@v1.3.3/mod.ts";
+export { default as ProgressBar } from "https://deno.land/x/progress@v1.3.3/mod.ts";
