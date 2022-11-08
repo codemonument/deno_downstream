178d578b2e0b21cefb2a16fb8df9e360cbadf812
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 05:11:07 +0100

    upgrade to progressbar 1.2.9 & remove custom fork

--- dependencies/_progressbar.ts
+++ dependencies/_progressbar.ts
@@ -1,6 +1,3 @@
-export * from "./progress-fork/mod.ts";
-export { default as ProgressBar } from "./progress-fork/mod.ts";
-
-// can be used in deno tests when this PR gets merged: https://github.com/deno-library/progress/pull/14
-// export * from "https://deno.land/x/progress@v1.2.8/mod.ts";
-// export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.8/mod.ts";
+// update to >1.3.0 when pr is merged
+export * from "https://deno.land/x/progress@v1.2.9/mod.ts";
+export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.9/mod.ts";
